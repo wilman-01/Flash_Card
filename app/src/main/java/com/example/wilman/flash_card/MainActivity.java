1f66c58d4163997416bc7a8497b2e1eafd6ff67c
wilman-01 <wilmangabriel2@gmail.com>
2018-10-27 13:57:21 -0500

    yay

--- app/src/main/java/com/example/wilman/flash_card/MainActivity.java
+++ app/src/main/java/com/example/wilman/flash_card/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.wilman.flash_card;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isShowingAnswers = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +69,34 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.green));
             }
         });
+
+
+        //findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View v) {
+
+        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswers == true) {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_eye);
+                    findViewById(R.id.answer_1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer_2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer_3).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = false;
+                }
+                else {
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.ic_eye_off);
+                    findViewById(R.id.answer_1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer_2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer_3).setVisibility(View.VISIBLE);
+                    isShowingAnswers = true;
+                }
+            }
+        });
+
+
+
 
     }
 
